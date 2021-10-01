1e04a34870f00b09fa4cf3878877c6374da27e48
Shaz <shahanawaj.patel@virtualclarity.com>
2021-10-01 17:26:00 +0530

    Initial commit

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  
 
-curl -i -u shahanawaj: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 --data '{"name":"${0}"}' https://api.github.com/user/repos
+echo "curl -i -u shahanawaj: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 --data '{"name":"${0}"}' https://api.github.com/user/repos"
