9c2a90850c7259821489a0fcdf1e423695b6bdfe
Shaz <shahanawaj.patel@virtualclarity.com>
2021-10-01 18:36:10 +0530

    Initial commit

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  
 repo_name=${1}
-curl -i -u shahanawaj:${2} --data '{"name":"'"$repo_name"'"}' https://api.github.com/user/repos
+curl -H "Authorization: token ${2}"  --data '{"name":"'"$repo_name"'"}' https://api.github.com/user/repos
